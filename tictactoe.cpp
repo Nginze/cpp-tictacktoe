8c76c2722fd640423243dedcd329740a42c7437b
Jonathan <KuugJonathan45@gmail.com>
2022-08-02 02:42:25 +0000

    game logic

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -8,17 +8,28 @@ void resetBoard();
 void playerMakeMove();
 void cpuMakeMove();
 bool isDraw();
-void hasWinner();
+char hasWinner();
 
 
 char board[3][3];
 
 int main()
 {   
-    resetBoard(); 
+    resetBoard();
     drawBoard();
-    playerMakeMove();
-    cpuMakeMove();
+    while(!isDraw()){
+        playerMakeMove();
+        if(hasWinner() != ' '){
+            cout << "player using" << hasWinner() << "wins";
+            break;
+        }
+        cpuMakeMove();
+        if(hasWinner() != ' '){
+            cout << "player using " << hasWinner() << " wins";
+            break;
+        }
+    }
+   
     return 0;    
 }
 
@@ -86,6 +97,21 @@ bool isDraw () {
     return true;
 }
 
-void hasWinner () {
-    
+char hasWinner () {
+    if(board[0][0] == board[1][1] && board[0][0] == board[2][2]){
+        return board[0][0];
+    }
+    if(board[2][0] == board[1][1] && board[2][0] == board[0][2]){
+        return board[2][0];
+    }
+    for(int i = 0; i < 3 ; i++){
+        if(board[i][0] == board[i][1] && board[i][0] == board[i][2]){
+            return board[i][0];
+        }
+        if(board[0][i] == board[1][i] && board[0][i] == board[2][i]){
+            return board[0][i];
+        }
+
+    }
+    return ' ';
 }
